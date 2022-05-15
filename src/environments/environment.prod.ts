211828dc75188d74c7af431c658438fe7f10b716
Ricardo Bohadana Martins <cacolorde@gmail.com>
2022-05-15 20:51:46 -0300

    changed api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://clienteapi-001-site1.htempurl.com/',
+  baseUrl: 'https://clientesapi.up.railway.app/',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'http://clienteapi-001-site1.htempurl.com/',
+  baseUrl: 'https://clientesapi.up.railway.app/',
 };
 
 /*
